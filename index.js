6c46da5dffd1ea2dd2d0f293e4e94bf5abefbdb7
isidrov31 <127324143+isidrov31@users.noreply.github.com>
2023-11-26 11:51:17 -0300

    primer commit

--- index.js
+++ index.js
@@ -1,7 +1,95 @@
 require('dotenv').config()
-
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
 const PORT = process.env.PORT
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const path = require('path');
 
-server.listen(PORT, function(){
-    console.log("Servidor corriendo en puerto: "+ PORT)
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() =>{
+    console.log("conexion exitosa a la base de datos")
+})
+.catch((error) => console.log("hubo un error al conectarse a la base de datos", { error}))
+
+const comidaSkema = new Schema({
+  name: String,
+  type: String,
+  done: Boolean
+})
+
+const Comida = mongoose.model("anime", comidaSkema, "dbComidas")
+
+
+app.use(bodyParser.json());
+
+//Servir archivos estaticos
+app.use(express.static('public'))
+// Sirve archivos estáticos de FontAwesome desde node_modules
+app.use('/static/fontawesome', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
+
+
+
+//Configurar Rutas//
+
+app.get('/api/comidas', (req, res) => {
+ Comida.find()
+ .then((comidas) => {
+  res.status(200).json({ok:true, data: comidas})
+ })
+ .catch((error) => {
+  res
+      .status(400)
+      .json({ok: false, message: "Hubo un problema al obtener las comidas"})
+ })
+})
+
+app.post('/api/comidas', (req, res) => {
+  const body = req.body
+  console.log({ body })
+  Comida.create({
+    name: body.name,
+    type: body.type,
+    done: false,
+  }).then((createdComida)=>{
+    res
+    .status(201)
+    .json ({ ok:true, message: "Comida agregada con exito", data: createdComida})
+  }).catch((error) => {
+    res.status(400).json({ok:false, message:"Error al agregar la comida"})
+  })
+})
+
+app.put('/api/comidas/:id', (req, res) => {
+  const body = req.body
+  const id = req.params.id
+  console.log({ body })
+  Comida.findByIdAndUpdate(id,{
+    name: body.name,
+    type: body.type,
+  }).then((updatedComida)=>{
+    res
+    .status(200)
+    .json ({ ok:true, message: "Comida actualizada con exito", data: updatedComida})
+  }).catch((error) => {
+    res.status(400).json({ok:false, message:"Error al actualizar la comida"})
+  })
+})
+
+app.delete('/api/comidas/:id', (req, res) => {
+  const id = req.params.id
+  Comida.findByIdAndDelete(id).then((deletedComida) => {
+    res.status(200).json({ok:true, data: deletedComida })
+  }).catch(() =>{
+    res.status(400).json({ok:false, message: "Hubo un error al eliminar la tarea"})
+  })
+})
+
+
+
+//Configurar Puerto//
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
---
+++ public/index.js
@@ -0,0 +1,106 @@
+//obtengo los elementos del html
+const addComida = document.getElementById("addComida");
+const nombreComida = document.getElementById("nombreComida");
+const tipoComida = document.getElementById("tipoComida");
+const comidasAgregadas = document.getElementById("comidasAgregadas")
+
+const baseUrl = "http://localhost:4001/api"
+
+let comidaEdit = null
+
+//le doy funcionalidad a los botones
+
+
+addComida.addEventListener("click", function(){
+    const creating = !comidaEdit
+    console.log("Agregar Comida")
+    console.log({nombreComida})
+    console.log({tipoComida})
+    const path = creating ? "comidas" : `comidas/${comidaEdit._id}`
+    const method = creating ? "POST" : "PUT"
+
+    fetch(`${baseUrl}/${path}`,{
+        method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: nombreComida.value, type: tipoComida.value }),
+    })
+    .then((res) => {
+        getComidas()
+        addComida.innerText = "Agregar Comida"
+        console.log({ res })
+        return res.json()
+    })
+    .then((resJSON) => {
+        console.log({ resJSON })
+    })
+    .catch((error) => {
+        console.log("Error al realizar la solicitud:", error);
+    });
+});
+
+function getComidas(){
+    comidasAgregadas.innerHTML = null
+    nombreComida.value = ""
+    fetch(`${baseUrl}/comidas`)
+    .then((res) => {
+        console.log({ res })
+        return res.json()
+    })
+    .then((resJSON) => {
+        console.log({ resJSON })
+        const comidas = resJSON.data
+        console.log(comidas)
+        for (const comida of comidas) {
+            const deleteComidaBtn = document.createElement('button')
+            const comidaContainer = document.createElement('div');
+            deleteComidaBtn.innerText = "Borrar"
+
+            // Mostrar el nombre de la comida
+            const nombreParagraph = document.createElement('p');
+            nombreParagraph.innerText = `${comida.name}`;
+            comidaContainer.appendChild(nombreParagraph);
+
+            // Agregar estilos de Bs5
+            comidaContainer.classList.add('card', 'mb-3', 'col-md-2', 'cardComida');
+            deleteComidaBtn.classList.add('btn', 'btn-sm', 'btn-danger', 'delete-button');
+
+            //Agregar icono
+            deleteComidaBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+
+            // Editar comida
+            nombreParagraph.addEventListener("click", (e) =>{
+                nombreComida.value = comida.name
+                addComida.innerHTML = "Modificar Comida"
+                comidaEdit = comida
+                console.log ({ comidaEdit })
+
+            })
+
+            // Mostrar el tipo de la comida
+            const tipoParagraph = document.createElement('p');
+            tipoParagraph.innerText = `Tipo: ${comida.type}`;
+            comidaContainer.appendChild(tipoParagraph);
+
+            // Configurar el ID
+            deleteComidaBtn.setAttribute('id', comida._id);
+
+            // funcion delete
+            deleteComidaBtn.addEventListener("click", function(e){
+                comidaid = e.target.id
+                deleteComidaBtn.innerText="..."
+                fetch(`${baseUrl}/comidas/${comidaid}`, {
+                    method: "DELETE",
+                }).then(() => {
+                const comidaDivDelete =  deleteComidaBtn.parentElement
+                comidaDivDelete.remove()
+                })
+            })
+
+            // Agregar el contenedor de comida al elemento contenedor en tu HTML
+            comidasAgregadas.appendChild(comidaContainer);
+            comidaContainer.appendChild(deleteComidaBtn);
+        }
+    });
+}
+
+getComidas()
